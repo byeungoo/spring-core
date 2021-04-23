14b06b2199d03a2c3c1d32e196019bc946d20559
byeungoo <dksekfldks65@naver.com>
2021-04-24 00:25:29 +0900

    bean scope 예제 작성

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value="request", proxyMode = ScopedProxyMode.TARGET_CLASS)  //request만 사용하면 스프링 컨테이너에서 빈 등록 후 의존성 주입할 때 주입할 빈이 없음. proxyMode로 설정해준다.
+public class MyLogger {
+
+    private String uuid;
+    private String requestUrl;
+
+    public void setRequestUrl(String requestUrl) {
+        this.requestUrl = requestUrl;
+    }
+
+    public void log(String message){
+        System.out.println("[" + uuid + "]" + "[" + requestUrl + "]" + message);
+    }
+
+    @PostConstruct
+    public void init(){
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create : " + this);
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+
+}
